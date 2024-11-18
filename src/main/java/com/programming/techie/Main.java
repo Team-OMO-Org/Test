6c66f8baaa6b9ca23acacda6761674128813a68e
MichaelJGKopp <michaeljg.kopp@gmail.com>
2024-11-18 23:03:22 +0100

    OMO-1 testCommitMsgOMO1

--- src/main/java/com/programming/techie/Main.java
+++ src/main/java/com/programming/techie/Main.java
@@ -2,6 +2,6 @@ package com.programming.techie;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world! World");
+    System.out.println("Hello world! World World");
   }
 }
